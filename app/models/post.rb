62d3ad87af09a581215a5df2e81da864dc694a52
Pal Makkar <pmakkar97@gmail.com>
2016-08-02 13:02:19 -0700

    added some validations to user and post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,14 @@
 class Post < ActiveRecord::Base
+	validates :content, presence: :true, length: { minimum: 4, maximum: 50 }
+	validates :title, presence: :true, length: { maximum: 150 }
+	validates :description, length: { maximum: 2000 }
+
+
+
+	default_scope -> { order('created_at DESC') }
+
+	#relationships
 	belongs_to :user, dependent: :destroy
-  default_scope -> { order('created_at DESC') }
 
   has_many :likes, foreign_key: "liked_id", dependent: :destroy
   has_many :liking_users, :through => :likes, source: :liker
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
+
 	before_save { self.email = email.downcase }
   validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :bio, length: { maximum: 2000 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                           format: { with: VALID_EMAIL_REGEX },
@@ -8,12 +11,8 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   
-  # def User.digest(string)
-  #   cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-  #                                                 BCrypt::Engine.cost
-  #   BCrypt::Password.create(string, cost: cost)
-  # end
   
+  #relationships
 	has_many :posts, dependent: :destroy
 
 	has_many :likes, foreign_key: "liker_id", dependent: :destroy
