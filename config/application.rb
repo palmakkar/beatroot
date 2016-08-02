dd7632b87263fa0ab18fd0aa91eea4ec36054d68
Pal Makkar <pmakkar97@gmail.com>
2016-08-02 12:48:30 -0700

    Inculuded secure passwords for user using password_digest

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module BeatRoot
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    I18n.enforce_available_locales = true
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
