db7c2362b4ac09de071c692e830ec53cdad459e8
hershalb <sunil@Hershals-MacBook-Pro.local>
2016-09-05 22:50:47 -0700

    added index to email

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802185826) do
+ActiveRecord::Schema.define(version: 20160906052104) do
 
   create_table "follows", force: :cascade do |t|
     t.integer  "follower_id"
@@ -44,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160802185826) do
     t.datetime "updated_at",      null: false
     t.string   "last_name"
     t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
