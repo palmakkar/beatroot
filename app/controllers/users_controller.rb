da70b0a70c36a8a804f9d136dc9fd90663f792e9
hershalb <sunil@Hershals-MacBook-Pro.local>
2016-09-06 02:30:25 -0700

    Added routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
     if @user.save
-      flash[:success] = "Signup successful!"
+      flash[:success] = "Welcome to BeatRoot!"
       redirect_to @user
     else
       render 'new'
@@ -17,8 +17,10 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
-  def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password,
-                                   :password_confirmation)
+  private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password,
+                                     :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-
-	before_save { self.email = email.downcase }
+  before_save { self.email = email.downcase }
+  
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
   validates :bio, length: { maximum: 2000 }
